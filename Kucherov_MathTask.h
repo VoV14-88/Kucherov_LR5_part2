ff06311207a28168d860726fd35ea87b101a6467
VoV14-88 <kucerovvova36@gmail.com>
2025-04-29 11:42:21 +0300

    1

---
+++ Kucherov_MathTask.h
@@ -0,0 +1,10 @@
+#ifndef SURNAME_MATHTASK_H
+#define SURNAME_MATHTASK_H
+
+#include <string>
+
+bool ValidateInput(const std::string& input);
+void EnterValue(int& value, const std::string& prompt);
+int CalculateRectangleArea(int a, int b);
+
+#endif
