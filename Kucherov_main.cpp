76e6202434caa331cde37f7c585457a6e998f57b
VoV14-88 <kucerovvova36@gmail.com>
2025-05-11 18:14:19 +0300

    add Kucherov_main.cpp

---
+++ Kucherov_main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include "Kucherov_MathTask.h"
+
+int main() {
+    int A = EnterNumber("Введите число A: ");
+    int B = EnterNumber("Введите число B: ");
+
+    std::cout << "A * B = " << Multiply(A, B) << "\n";
+    
+    try {
+        std::cout << "A / B = " << Divide(A, B) << "\n";
+    } catch (const std::exception& e) {
+        std::cerr << "Ошибка: " << e.what() << "\n";
+    }
+
+    return 0;
+}
